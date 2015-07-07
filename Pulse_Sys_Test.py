5cb2d56db05230c83c3506d4c4bb9e754c16635b
Aaron Parsons <aparsons@berkeley.edu>
2015-07-07 11:11:44 -0700

    Pulse_Sys_Test cleaned up to follow coding conventions

--- Pulse_Sys_Test.py
+++ Pulse_Sys_Test.py
@@ -1,110 +1,57 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt, numpy as np
 
-# Number of test points
-N  = 50  
+N  = 500 # Number of test points
+PLS = 20 # pulse location
 
-# SYSTEM UNDER TEST (DO NOT KNOW) Use any # to argue
-def Tri(x):
-    sY = np.array(range(0,50))
-    sY[25:] = sY[24::-1]
-    sY[16:35] = 15
-    return sY
+def bandpass(x):
+    '''SYSTEM UNDER TEST (DO NOT KNOW) Use any # to argue'''
+    bp = np.arange(N)
+    bp[N/2:] = bp[N/2-1::-1]
+    bp[16:-15] = 15
+    return bp
 
-# Fourier Transform of Signal
-# Select 1 for Inverse FFT
-def Transform(signal, inverse):
-    if inverse == 1:
-        IFFT = np.fft.ifft(signal)
-        return IFFT
-    else:
-        FFT = np.fft.fft(signal)
-        return FFT
+def fft_graph(data):
+    '''Converts FFT data for Graphing'''
+    return np.fft.fftshift(np.abs(data))
 
-# Converts FFT for Graphing 
-# Only Use 1 for 
-def FFT_Graph(FFT):
-    FFTG = np.fft.fftshift(abs(FFT))
-   # if x == 1:
-      #  FFTG = FFTG[N/2:]
-    return FFTG
+t = np.linspace(0,30*np.pi, N)
+v = np.fft.fftshift(np.fft.fftfreq(t.size, t[1] - t[0]))
 
-# Creates Freq points for Freq Domain
-def Freq_points(tx):
-    a = np.fft.fftshift(np.fft.fftfreq(tx.size, tx[1] - tx[0]))
-    #a = a[N/2:]
-    return a
+p = np.zeros(N); p[PLS] = 1 # Pulse Generator(Pulse @ bin 0)
+vp = np.fft.fft(p) # FFT of Pulse -> Constant line across all v
+vs = bandpass(1) # bandpass(1) is in frequency domain and is assumed np.fft.ffted
+vps = vp * vs # Pulse is sent into system and gets multiplied, Convolution THM T{p o s} = vp * vs
 
-
-tx = np.linspace(0,30*np.pi, N)
-vx = Freq_points(tx)
-
-"""
---------------Pulse Generation---------------
-"""
-# Pulse Generator(Pulse @ bin 0)
-p = np.zeros(N)
-p[20] = 1
-
-# FFT of Pulse -> Constant line across all vx
-Tp = Transform(p, 0)
-
-"""
---------------TESTING SYSTEM----------------
-"""
-# Tri(1) is in frequency domain and is assumed Transformed
-Ts = Tri(1)
-
-# Pulse is sent into system and gets multiplied
-# Convolution THM T{p o s} = Tp * Ts
-out = Tp * Ts
-
-"""
-------------------ANALYSIS------------------
-"""
-# Take out and Inverse Transform it to produce the convolution of p o s
-# ITout is in time domain
-ITout = Transform(out, 1)
-Slice = ITout[20:]  
-Part = ITout[:20]
-
-# New output in time domain w/ pulse moved
-Output = np.zeros(N)
-Output[:30] = Slice
-Output[30:] = Part
-
-Sys_Int = Transform(Output, 0)
-
+# Take vps and Inverse np.fft.fft it to produce the convolution of p o s
+# ps is in time domain
+ps = np.fft.ifft(vps)
+ps_d = np.concatenate([ps[PLS:],ps[:PLS]])
+bp = np.fft.fft(ps_d) # New vpsput in time domain w/ pulse moved
 
 # All Graphs in order of action (Each Graph has Description)
-
 plt.figure(6)
 plt.plot(p, 'r')
-plt.title('Pulse in Time Doamin Before Test')
+plt.title('Pulse in Time Domain Before Test')
 
-TpG = FFT_Graph(Tp)
 plt.figure(5)
-plt.plot(vx,TpG, 'r')
+plt.plot(v, vp.real, v, vp.imag)
+plt.plot(v, fft_graph(vp), 'r')
 plt.title('Pulse in Frequency Domain Before Test')
 
-outG = FFT_Graph(out)
 plt.figure(4)
-plt.plot(vx, outG, 'b')
-plt.title("Pulse multiplied w/ System in Frequency Domain (Output)")
+plt.plot(v, fft_graph(vps), 'b')
+plt.title("Pulse multiplied w/ System in Frequency Domain (ps_d)")
 
 plt.figure(3)
-plt.plot(ITout, 'g')
-plt.title('Inverse Transform of output which gives Convolution of Pulse & System')
+plt.plot(ps, 'g')
+plt.title('Inverse np.fft.fft of vpsput which gives Convolution of Pulse & System')
 
 plt.figure(2)
-plt.plot(Output, 'y')
-
-Sys_IntG = FFT_Graph(Sys_Int)
-plt.figure(1)
-plt.plot(Sys_IntG, 'm')
+plt.plot(ps_d, 'y')
 
 plt.figure(1)
-plt.plot(Tri(1), 'k')
+#plt.plot(fft_graph(bp), 'm')
+plt.plot(bp, 'm')
+plt.plot(bandpass(1), 'k')
 
-plt.grid()
 plt.show()
