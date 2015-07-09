f3ee02432cddd788d233e531dfdcb7866a594980
Manuel <paul.manuel.pasqual@gmail.com>
2015-07-09 16:42:09 -0700

    Pulse Calibration Simulation [Final] CONDENSED

--- Pulse_Sys_Test.py
+++ Pulse_Sys_Test.py
@@ -7,32 +7,29 @@ def bandpass(x):
     '''SYSTEM UNDER TEST (DO NOT KNOW) Use any # to argue'''
     return np.sin(49*t) + np.sin(69*t)
 
-def fft_graph(data):
-    '''Converts FFT data for Graphing'''
-    return np.fft.fftshift(np.abs(data))
-
 t = np.linspace(0,30*np.pi, N) # x-axis of Time Domain
 v = np.fft.fftshift(np.fft.fftfreq(t.size, t[1] - t[0])) # x-axis of Frequency Domain
-p = np.zeros(N); p[PLS] = 1 # Pulse Generator(Pulse @ bin 0)
-vp = np.fft.fft(p) # FFT of Pulse -> Constant line across all v
-vs = bandpass(1) # bandpass(1) is in frequency domain and is assumed np.fft.ffted
+p = np.zeros(N); p[PLS] = 1 # Pulse Generator(Pulse @ bin 20)
+vp = np.fft.fft(p) 
+vs = bandpass(1) # bandpass(1) is in frequency domain and is assumed Fourier Transformed
 vps = vp * vs # Pulse is sent into system and gets multiplied, Convolution THM T{p o s} = vp * vs
 ps = np.fft.ifft(vps) # Take vps and Inverse Fourier Transform it to produce the convolution of p o s 
-ps_d = np.concatenate([ps[PLS:],ps[:PLS]]) # ps is in time domain
-bp = np.fft.fft(ps_d) # New vpsput in time domain w/ pulse moved
+ps_d = np.concatenate([ps[PLS:],ps[:PLS]]) # Deconvolve ps; ps is in time domain
+bp = np.fft.fft(ps_d) # Bandpass derived from Convolution THM
 
-# All Graphs in order of action (Each Graph has Description)
+# Remove Multi-Line comment to show graphs
+"""
 plt.figure(6)
 plt.plot(p, 'r')
 plt.title('Pulse in Time Domain Before Test')
 
 plt.figure(5)
 plt.plot(v, vp.real, v, vp.imag)
-plt.plot(v, fft_graph(vp), 'r')
+plt.plot(v, np.fft.fftshift(np.abs(vp)), 'r')
 plt.title('Pulse in Frequency Domain Before Test')
 
 plt.figure(4)
-plt.plot(v, fft_graph(vps), 'k')
+plt.plot(v, np.fft.fftshift(np.abs(vps)), 'k')
 plt.plot(v, vps.real, v, vps.imag)
 plt.title("Pulse multiplied w/ System in Frequency Domain ")
 
@@ -50,3 +47,4 @@ plt.plot(v, bandpass(1), 'k')
 plt.title('The System Interference in Frequency Domain')
 
 plt.show()
+"""
