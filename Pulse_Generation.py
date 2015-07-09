cb4fc41133af3e37cd8feaf3fd2f4b05e4f903b6
Manuel <paul.manuel.pasqual@gmail.com>
2015-07-09 16:25:00 -0700

    Pulse Generator Simulation [Final] CONDENSED

--- Pulse_Generation.py
+++ Pulse_Generation.py
@@ -1,53 +1,27 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt, numpy as np
 
-# Number of test points
-N  = 50        
-
-# Fourier Transform of Signal
-# Select 1 for Inverse FFT
-def Transform(signal, inverse):
-    if inverse == 1:
-        IFFT = np.fft.ifft(signal)
-        return IFFT
-    else:
-        FFT = np.fft.fft(signal)
-        return FFT
-
-# Converts FFT for Graphing 
-def FFT_Graph(FFT):
-    FFTG = np.fft.fftshift(abs(FFT))
-    FFTG = FFTG[N/2:]
-    return FFTG
-
-# Creates Freq points for Freq Domain
-def Freq_points(tx):
-    a = np.fft.fftshift(np.fft.fftfreq(tx.size, tx[1] - tx[0]))
-    a = a[N/2:]
-    return a
-
-# Pulse Generator(Pulse @ bin 20)
-tx = np.linspace(0,30*np.pi, N)
-vx = Freq_points(tx)
-
-y = np.zeros(N)
-y[20] = 1
-
-# FFT of Pulse -> Constant line across all vx
-Ty = Transform(y, 0)
-
-
-TyG = FFT_Graph(Ty)
+N  = 500 # Number of test points  
+t = np.linspace(0,2*np.pi, N) # x-axis in time domain
+v = np.fft.fftshift(np.fft.fftfreq(t.size, t[1] - t[0])) # x-axis in frequency domain
+p = np.zeros(N)
+_p = np.zeros(N)
+p[0] = 1 # Pulse @ bin 0
+_p[20] = 1 # Pulse @ bin 20
+vp = np.fft.fft(p) #
+v_p = np.fft.fft(_p) 
 
 plt.figure(1)
-plt.plot(vx, TyG,'g')
-plt.xlabel('Frequency')
-plt.title('Frequency Domain')
+plt.plot(v, vp.real, v, vp.imag)
+plt.plot(v, vp, 'm')
+plt.title('Pulse @ bin 0')
 
 plt.figure(2)
-plt.plot(tx,y,'b')
-plt.xlabel('Time')
-plt.title("Time Domain")
+plt.plot(v, v_p.real, v, v_p.imag)
+plt.plot(v, np.fft.fftshift(np.abs(v_p)), 'k')
+plt.title('Pulse @ bin 20')
 
-plt.grid()
+plt.figure(3)
+plt.plot(t, p,'b', t,_p,'r')
+plt.title("Time Domain of both Pulses")
+
 plt.show()
